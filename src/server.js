3a62773b1b1d7c09275b308f4243522d2b3f1353
Atharva062006 <72563240+Atharva062006@users.noreply.github.com>
2026-06-11 10:59:12 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+
+// Error handling middleware
+
+// Testing postgres Connection
+app.get("/", async(req, res) => {
+    const result = await pool.query("SELECT current_database()");
+    res.send(`The database name is ${result.rows[0].current_database}`)
+});
+
+// Start the server
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`);
+});
